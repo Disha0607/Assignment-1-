487faa8f527392a60cf7d7bc829e22ac0d0d285f
Disha0607 <dpatel124@stevens.edu>
2025-09-10 20:04:43 -0400

    Update pay.py

--- pay.py
+++ pay.py
@@ -1,9 +1,5 @@
-def unfair_weekly_paycheck_amount(hours):
-    return hours * 7.5
-
-def fair_weekly_paycheck_amount(hours):
-    if hours <= 40:
-        return hours * 7.5
-    else:
-        overtime = hours - 40
-        return (40 * 7.5) + (overtime * 11.25)
+# pay.py
+# For fair pay
+hours = float(input())
+rate = float(input())
+print(hours * rate)
