c2124c4f7542ee77e88d5d2830e4c895ea6565cd
Disha0607 <dpatel124@stevens.edu>
2025-09-10 20:39:03 -0400

    Update pay.py

--- pay.py
+++ pay.py
@@ -1,21 +1,12 @@
-# GETTING USER INPUT
-number_of_hours = float(input("ENTER THE NUMBERS OF HOURS YOU HAVE WORKED = "))
+def fair_weekly_paycheck_amount(hours):
+    return hours * 30
 
-# DEFINING A FUNCTION
-def unfair_weekly_paycheck_amount(number_of_hours):
-    # rounded hours
-    rounded_hours = int(number_of_hours)
-    pay = 15 * rounded_hours
-    # RETURNING THE PAY
-    return pay
+def unfair_weekly_paycheck_amount(hours):
+    rounded_hours = int(hours)
+    return rounded_hours * 15
 
-# CALLING THE FUNCTION
-total_pay = unfair_weekly_paycheck_amount(number_of_hours)
-print("THE UNFAIR WEEKLY AMOUNT =", total_pay)
-
-def fair_weekly_paycheck_amount(n_hours):
-    payrate = 15 * n_hours
-    return payrate
-
-payamount = fair_weekly_paycheck_amount(number_of_hours)
-print("THE FAIR WEEKLY AMOUNT =", payamount)
+if __name__ == "__main__":
+    # Sample usage
+    hrs = float(input("Enter number of hours worked: "))
+    print("Fair weekly paycheck amount:", fair_weekly_paycheck_amount(hrs))
+    print("Unfair weekly paycheck amount:", unfair_weekly_paycheck_amount(hrs))
