281fbe3dcfc30ce9850daa2368e351419848359e
Disha0607 <dpatel124@stevens.edu>
2025-09-10 20:05:01 -0400

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,24 +1,12 @@
-#Getting the input from the user about the grades
-grades = float(input("ENTER YOUR GRADES="))
-
-#function declaration
+# grade.py
 def calculate(grade):
-    if 90 <= grade <= 100:
+    if grade >= 90:
         return "A"
-    elif 80 <= grade < 90:
+    elif grade >= 80:
         return "B"
-    elif 70 <= grade < 80:
+    elif grade >= 70:
         return "C"
-    elif 60 <= grade < 70:
+    elif grade >= 60:
         return "D"
-    elif 0 <= grade < 60:
-        return "F"
     else:
-        return None
-
-
-#calling a function
-finalgrades = calculate(grades)
-
-print("YOUR GRADES WILL BE=", finalgrades)
-
+        return "F"
